90ac585ee9a25815d7a874fadce6333e21bfa08a
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-10-24 22:03:43 +0700

    add repository

--- type-orm-demo/src/app.controller.ts
+++ type-orm-demo/src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { User } from './user.entity';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getUsers(): Promise<User[]> {
+    return this.appService.getAll();
   }
+
+  // Do More Endpoint
 }
--- type-orm-demo/src/app.service.ts
+++ type-orm-demo/src/app.service.ts
@@ -1,8 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    constructor(
+        @InjectRepository(User) private usersRepository: Repository<User>,
+    ) {}
+
+    async getAll(): Promise<User[]> {
+        return this.usersRepository.find();
+    }
+
+    async getOneById(id: number): Promise<User> {
+        try {
+            const user = await this.usersRepository.findOneOrFail(id);
+            return user;
+        } catch (error) {
+            throw new NotFoundException();
+        }
+    }
+
+    async createUser(
+        name: string,
+        email: string,
+        favorite: string,
+    ): Promise<User> {
+        const newUser = await this.usersRepository.create({
+            name,
+            email,
+            favorite,
+        });
+
+        return this.usersRepository.save(newUser);
+    }
+
+    async updateUser(name: string, id: number): Promise<User> {
+        const user = await this.getOneById(id);
+        user.name = name;
+        return this.usersRepository.save(user);
+    }
+
+    async deleteUser(id: number): Promise<User> {
+        const user: User = await this.getOneById(id);
+
+        return this.usersRepository.remove(user);
+    }
+
+    getHello(): string {
+        return 'Hello World!';
+    }
 }
