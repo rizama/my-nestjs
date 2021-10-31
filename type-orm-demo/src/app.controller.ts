36db44ee277743471116a11845b9f6aa2d6d9dd5
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-10-31 17:51:09 +0700

    chore: add controller

--- type-orm-demo/src/app.controller.ts
+++ type-orm-demo/src/app.controller.ts
@@ -1,15 +1,46 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateuserDto } from './dto/user-create.dto';
 import { User } from './user.entity';
 
-@Controller()
+@Controller('users')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  async getUsers(): Promise<User[]> {
-    return this.appService.getAll();
-  }
+    @Get()
+    async getUsers(): Promise<User[]> {
+        return this.appService.getAll();
+    }
 
-  // Do More Endpoint
+    @Get('/:id')
+    async getUser(@Param('id') id: number): Promise<User> {
+        return this.appService.getOneById(id);
+    }
+
+    @Post()
+    async storeUser(@Body() createUserDto: CreateuserDto): Promise<User> {
+        return this.appService.createUser(createUserDto);
+    }
+
+    @Patch('/:id')
+    async updateUser(
+        @Param('id') id: number,
+        @Body() updateUserDto: CreateuserDto,
+    ): Promise<User> {
+        const { name } = updateUserDto;
+        return this.appService.updateUser(name, id);
+    }
+
+    @Delete('/:id')
+    async deleteUser(@Param('id') id: number): Promise<User> {
+        return this.appService.deleteUser(id);
+    }
 }
