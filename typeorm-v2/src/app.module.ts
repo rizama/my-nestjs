b8a2abc52b933044f385649b6ad7bac2b926b33b
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-11-03 23:09:50 +0700

    chore: implement config orm

--- typeorm-v2/src/app.module.ts
+++ typeorm-v2/src/app.module.ts
@@ -3,19 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
+import { typeOrmConfig } from 'typeOrmConfig';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'typeorm-v2',
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(typeOrmConfig),
     EmployeesModule,
   ],
   controllers: [AppController],
--- typeorm-v2/src/employees/employees.module.ts
+++ typeorm-v2/src/employees/employees.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Employee } from './employee.entity';
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([Employee])]
+})
 export class EmployeesModule {}
