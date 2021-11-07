3d1b901e256c3b04192d92d6f972f8ed48871997
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-11-07 21:50:43 +0700

    chore: add meeting module

--- typeorm-v2/src/app.module.ts
+++ typeorm-v2/src/app.module.ts
@@ -5,12 +5,14 @@ import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
 import { typeOrmConfig } from 'typeOrmConfig';
 import { TasksModule } from './tasks/tasks.module';
+import { MeetingsModule } from './meetings/meetings.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     EmployeesModule,
     TasksModule,
+    MeetingsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
