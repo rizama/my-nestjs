cc841ce58a466851c5509dd962a7b31bd9ceac5a
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-11-10 06:52:33 +0700

    feat: add seeder

--- typeorm-v2/src/app.service.ts
+++ typeorm-v2/src/app.service.ts
@@ -1,8 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ContactInfo } from './employees/contact.entity';
+import { Employee } from './employees/employee.entity';
+import { Meeting } from './meetings/meeting.entity';
+import { Task } from './tasks/task.entity';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    constructor(
+        @InjectRepository(Employee) private employeeRepo: Repository<Employee>,
+        @InjectRepository(ContactInfo)
+        private contactInfoRepo: Repository<ContactInfo>,
+        @InjectRepository(Meeting) private meetingRepo: Repository<Meeting>,
+        @InjectRepository(Task) private taskRepo: Repository<Task>,
+    ) {}
+
+    async seed() {
+        // Employee CEO
+        const ceo = this.employeeRepo.create({ name: 'Mr. Ceo' });
+        await this.employeeRepo.save(ceo);
+
+        // ContactInfo CEO & Relation
+        const ceoContactInfo = this.contactInfoRepo.create({
+            email: 'ceo@mail.com',
+        });
+        ceoContactInfo.employee = ceo;
+        await this.contactInfoRepo.save(ceoContactInfo);
+
+        // Employee CEO
+        const manager = this.employeeRepo.create({ name: 'Sam', manager: ceo });
+        await this.employeeRepo.save(manager);
+
+        const task1 = this.taskRepo.create({ name: 'Hire people' });
+        await this.taskRepo.save(task1)
+        const task2 = this.taskRepo.create({ name: 'Present to CEO' });
+        await this.taskRepo.save(task2)
+
+        manager.tasks = [task1, task2];
+
+        const meeting1 = this.meetingRepo.create({zoomUrl: 'lalala@meeting.com'})
+        meeting1.attendees = [ceo];
+        await this.meetingRepo.save(meeting1);
+
+        manager.meetings = [meeting1]
+
+        await this.employeeRepo.save(manager);
+    }
+
+    getHello(): string {
+        return 'Hello World!';
+    }
 }
