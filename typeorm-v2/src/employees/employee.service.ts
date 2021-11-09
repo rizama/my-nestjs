10cb0c9730eb818eddb0c2749cc40f76043cca47
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-11-10 06:52:14 +0700

    chore: add injection employee service

--- typeorm-v2/src/employees/employee.service.ts
+++ typeorm-v2/src/employees/employee.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { ContactInfo } from './contact.entity';
 import { Employee } from './employee.entity';
 
 @Injectable()
 export class EmployeeService {
-    constructor(@InjectRepository(Employee) private employeeRepo: Repository<Employee>){}
-    
+    constructor(
+        @InjectRepository(Employee) private employeeRepo: Repository<Employee>,
+        @InjectRepository(ContactInfo)
+        private contactInfoRepo: Repository<ContactInfo>,
+    ) {}
 }
