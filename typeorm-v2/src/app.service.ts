e6405d639c0cd47744c2964232f3ee8e2f87e94d
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-11-17 22:09:34 +0700

    chore: update services

--- typeorm-v2/src/app.service.ts
+++ typeorm-v2/src/app.service.ts
@@ -33,22 +33,51 @@ export class AppService {
         await this.employeeRepo.save(manager);
 
         const task1 = this.taskRepo.create({ name: 'Hire people' });
-        await this.taskRepo.save(task1)
+        await this.taskRepo.save(task1);
         const task2 = this.taskRepo.create({ name: 'Present to CEO' });
-        await this.taskRepo.save(task2)
+        await this.taskRepo.save(task2);
 
         manager.tasks = [task1, task2];
 
-        const meeting1 = this.meetingRepo.create({zoomUrl: 'lalala@meeting.com'})
+        const meeting1 = this.meetingRepo.create({
+            zoomUrl: 'lalala@meeting.com',
+        });
         meeting1.attendees = [ceo];
         await this.meetingRepo.save(meeting1);
 
-        manager.meetings = [meeting1]
+        manager.meetings = [meeting1];
 
         await this.employeeRepo.save(manager);
     }
 
     getHello(): string {
         return 'Hello World!';
+    }
+
+    async getEmployeeById(id: number): Promise<Employee> {
+        // Eloquent
+        // return this.employeeRepo.findOne(id, {
+        //     relations: [
+        //         'manager',
+        //         'directReports',
+        //         'tasks',
+        //         'contactInfo',
+        //         'meetings',
+        //     ],
+        // });
+
+        // For Complex Query
+        return this.employeeRepo
+            .createQueryBuilder('employee')
+            .leftJoinAndSelect('employee.contactInfo', 'contactInfo')
+            .leftJoinAndSelect('employee.directReports', 'directReports')
+            .leftJoinAndSelect('employee.meetings', 'meetings')
+            .leftJoinAndSelect('employee.tasks', 'tasks')
+            .where('employee.id = :employeeId', { employeeId: id })
+            .getOne();
+    }
+
+    async deleteEmployee(id: number): Promise<void> {
+        this.employeeRepo.delete(id);
     }
 }
