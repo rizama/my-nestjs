de7278dc0827881c4415ad92f5e059d1f922d368
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-11-17 22:09:18 +0700

    chore: add controller

--- typeorm-v2/src/app.controller.ts
+++ typeorm-v2/src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Employee } from './employees/employee.entity';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
+
+    @Get('seeder')
+    async seedData(): Promise<void> {
+        this.appService.seed();
+        console.log('Seed Success!');
+    }
+
+    @Get('/:id')
+    async getEmployeeById(@Param('id') id: number): Promise<Employee> {
+        return this.appService.getEmployeeById(id);
+    }
+
+    @Get('/delete/:id')
+    async deleteEmployeeById(@Param('id') id: number): Promise<void> {
+        this.appService.deleteEmployee(id);
+        console.log('Deleted');
+    }
 }
