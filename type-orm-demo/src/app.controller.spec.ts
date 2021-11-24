a5a32ff104eddea844d3b833bf81ffe1cb5b53ac
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-11-24 22:46:04 +0700

    test: refactor test controller

--- type-orm-demo/src/app.controller.spec.ts
+++ type-orm-demo/src/app.controller.spec.ts
@@ -47,7 +47,7 @@ describe('AppController', () => {
         deleteUser: jest.fn((id) => {
             return {
                 id,
-                ...data[0]
+                ...data[0],
             };
         }),
     };
@@ -64,62 +64,60 @@ describe('AppController', () => {
         appController = app.get<AppController>(AppController);
     });
 
-    describe('Users Controller', () => {
-        it('controller should be defined"', () => {
-            expect(appController).toBeDefined();
+    it('controller should be defined"', () => {
+        expect(appController).toBeDefined();
+    });
+
+    it('should fetch all users', async () => {
+        expect(await appController.getUsers()).toEqual(data);
+        expect(mockUserService.getAll).toHaveBeenCalled();
+    });
+
+    it('should fetch user', async () => {
+        const id = 1;
+        const singleData = {
+            id,
+            name: 'sam',
+            email: 'sam@gmail.com',
+            favorite: 'game',
+        };
+
+        expect(await appController.getUser(id)).toEqual(singleData);
+        expect(mockUserService.getOneById).toHaveBeenCalled();
+    });
+
+    it('should create a new user', async () => {
+        const CreateuserDto: CreateuserDto = {
+            name: 'sam',
+            email: 'sam@gmail.com',
+            favorite: 'game',
+        };
+
+        expect(await appController.storeUser(CreateuserDto)).toEqual(
+            CreateuserDto,
+        );
+        expect(mockUserService.createUser).toHaveBeenCalled();
+    });
+
+    it('should update a new user', async () => {
+        const updateUserDto = {
+            name: 'rizky',
+            email: 'sam@gmail.com',
+            favorite: 'game',
+        };
+        const id = 1;
+
+        expect(await appController.updateUser(id, updateUserDto)).toEqual({
+            id: 1,
+            ...updateUserDto,
         });
+        expect(mockUserService.updateUser).toHaveBeenCalled();
+    });
 
-        it('should fetch all users', async () => {
-            expect(await appController.getUsers()).toEqual(data);
-            expect(mockUserService.getAll).toHaveBeenCalled();
-        });
+    it('should delete user', async () => {
+        const id = 1;
 
-        it('should fetch user', async () => {
-            const id = 1;
-            const singleData = {
-                id,
-                name: 'sam',
-                email: 'sam@gmail.com',
-                favorite: 'game',
-            };
-
-            expect(await appController.getUser(id)).toEqual(singleData);
-            expect(mockUserService.getOneById).toHaveBeenCalled();
-        });
-
-        it('should create a new user', async () => {
-            const CreateuserDto: CreateuserDto = {
-                name: 'sam',
-                email: 'sam@gmail.com',
-                favorite: 'game',
-            };
-
-            expect(await appController.storeUser(CreateuserDto)).toEqual(
-                CreateuserDto,
-            );
-            expect(mockUserService.createUser).toHaveBeenCalled();
-        });
-
-        it('should update a new user', async () => {
-            const updateUserDto = {
-                name: 'rizky',
-                email: 'sam@gmail.com',
-                favorite: 'game',
-            };
-            const id = 1;
-
-            expect(await appController.updateUser(id, updateUserDto)).toEqual({
-                id: 1,
-                ...updateUserDto,
-            });
-            expect(mockUserService.updateUser).toHaveBeenCalled();
-        });
-
-        it('should delete user', async () => {
-            const id = 1;
-
-            expect(await appController.deleteUser(id)).toEqual(data[0]);
-            expect(mockUserService.deleteUser).toHaveBeenCalled();
-        });
+        expect(await appController.deleteUser(id)).toEqual(data[0]);
+        expect(mockUserService.deleteUser).toHaveBeenCalled();
     });
 });
