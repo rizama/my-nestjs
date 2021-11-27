370bfc646c9e86576ca1046ced8534fc158f82c9
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-11-27 11:48:29 +0700

    test: create user service

--- type-orm-demo/src/app.service.spec.ts
+++ type-orm-demo/src/app.service.spec.ts
@@ -1,23 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 import { User } from './user.entity';
 
 describe('AppController', () => {
     let service: AppService;
 
-    const mockUserRepository = {};
+    const mockUserRepository = {
+        create: jest.fn().mockImplementation((dto) => dto),
+        save: jest.fn().mockImplementation((user) =>
+            Promise.resolve({
+                id: Date.now(),
+                ...user,
+            }),
+        ),
+    };
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [AppService],
-        }).overrideProvider(AppService)
-        .useValue(mockUserRepository)
-        .compile();
+            providers: [
+                AppService,
+                {
+                    provide: getRepositoryToken(User),
+                    useValue: mockUserRepository,
+                },
+            ],
+        }).compile();
 
         service = module.get<AppService>(AppService);
     });
 
     it('should be defined', () => {
         expect(service).toBeDefined();
+    });
+
+    it('should create user and return it', async () => {
+        expect(
+            await service.createUser({
+                name: 'sam',
+                email: 'sam@gmail.com',
+                favorite: 'game',
+            }),
+        ).toEqual({
+            id: expect.any(Number),
+            name: 'sam',
+            email: 'sam@gmail.com',
+            favorite: 'game',
+        });
+
+        expect(mockUserRepository.create).toHaveBeenCalled();
     });
 });
