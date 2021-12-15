0540ad904c209d06d70df5dd0e7b76725a9e4aa2
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-12-15 22:05:42 +0700

    feat: initial amazon module

---
+++ amazon-price-scrape/src/amazon/amazon.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('amazon')
+export class AmazonController {}
--- amazon-price-scrape/src/app.module.ts
+++ amazon-price-scrape/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AmazonController } from './amazon/amazon.controller';
+import { AmazonService } from './amazon/amazon.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AmazonController],
+  providers: [AppService, AmazonService],
 })
 export class AppModule {}
