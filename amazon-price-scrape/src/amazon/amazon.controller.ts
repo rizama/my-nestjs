de7883fbe00430dd5cad2dad9d6907b611ebe995
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-12-16 23:03:42 +0700

    add controller and services

--- amazon-price-scrape/src/amazon/amazon.controller.ts
+++ amazon-price-scrape/src/amazon/amazon.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AmazonPayload } from './amazon.mode';
+import { AmazonService } from './amazon.service';
 
 @Controller('amazon')
-export class AmazonController {}
+export class AmazonController {
+    constructor(private readonly amazonService: AmazonService) {}
+
+    @Post()
+    getPrice(@Body() body: AmazonPayload) {
+        const { product_id } = body;
+        return this.amazonService.getPrice(product_id);
+    }
+}
--- amazon-price-scrape/src/amazon/amazon.service.ts
+++ amazon-price-scrape/src/amazon/amazon.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class AmazonService {}
+export class AmazonService {
+    async getPrice(productId: string) {
+        try {
+            const productUrl = this.getProduct(productId);
+            console.log(encodeURI(productUrl));
+            const { data } = await axios.get(productUrl, {
+                headers: {
+                    Accept: 'text/html,*/*',
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0',
+                    // TE: 'trailers',
+                    // Host: 'www.amazon.com',
+                    // 'Sec-Fetch-Mode': 'cors'
+                },
+            });
+
+            console.log(data);
+            // return productUrl;
+        } catch (error) {
+            console.log(error.stack);
+        }
+    }
+
+    getProduct(productId: string) {
+        return `https://www.amazon.com/gp/aod/ajax/?asin=${productId}&m=&qid=&smid=&sourcecustomerorglistid=&sourcecustomerorglistitemid=&sr=&pc=dp`;
+    }
+}
