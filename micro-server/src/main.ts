f21f11dd26e9af6676c7dc6645c254296c57ca9f
Rizky Sam Pratama <rizkysampratama@gmail.com>
2022-05-10 23:03:28 +0700

    setup grpc

--- micro-server/src/main.ts
+++ micro-server/src/main.ts
@@ -1,6 +1,7 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 // Create a logger instance
@@ -8,14 +9,24 @@ const logger = new Logger('Main');
 
 // Create the microservice options
 const microserviceOptions = {
+  // For TCP
   // transport: Transport.TCP,
   // options: {
   //   host: '127.0.0.1',
   //   port: 8877,
   // },
-  transport: Transport.REDIS,
+
+  // For REDIS
+  // transport: Transport.REDIS,
+  // options: {
+  //   url: 'redis://localhost:6379/',
+  // },
+
+  // For GRPC
+  transport: Transport.GRPC,
   options: {
-    url: 'redis://localhost:6379/',
+    package: 'app',
+    protoPath: join(__dirname, '../src/app.proto'),
   },
 };
 
