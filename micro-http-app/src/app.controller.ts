791d4ae0c43784fbadcad44738b5a5aea4ff4424
Rizky Sam Pratama <rizkysampratama@gmail.com>
2022-05-11 23:16:27 +0700

    implement grpc

--- micro-http-app/src/app.controller.ts
+++ micro-http-app/src/app.controller.ts
@@ -1,44 +1,66 @@
 import { Body, Controller, Get, Logger, Post } from '@nestjs/common';
 import {
-  ClientOptions,
-  ClientProxyFactory,
-  Transport,
+    Client,
+    ClientGrpc,
+    ClientOptions,
+    ClientProxyFactory,
+    Transport,
 } from '@nestjs/microservices';
 import { AppService } from './app.service';
+import { IGrpcService } from './grpc.interface';
+import { microserviceOptions } from './grpc.options';
 import { MathService } from './math.service';
 
 @Controller()
 export class AppController {
-  // Create a logger instance
-  private logger = new Logger('AppController');
+    // Create a logger instance
+    private logger = new Logger('AppController');
 
-  constructor(
-    private readonly appService: AppService,
-    private readonly mathService: MathService
-  ) {}
+    @Client(microserviceOptions)
+    private client: ClientGrpc;
 
-  private microserviceOptions: ClientOptions = {
-    transport: Transport.TCP,
-    options: {
-      host: '127.0.0.1',
-      port: 8877,
-    },
-  };
+    private grpcService: IGrpcService;
 
-  private client = ClientProxyFactory.create(this.microserviceOptions);
+    // constructor(
+    //   private readonly appService: AppService,
+    //   private readonly mathService: MathService
+    // ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    // private microserviceOptions: ClientOptions = {
+    //   transport: Transport.TCP,
+    //   options: {
+    //     host: '127.0.0.1',
+    //     port: 8877,
+    //   },
+    // };
 
-  // Map the POST /add routeto this method
-  @Post('add')
-  // define the logic to be executed
-  async accumulate(@Body('data') data: number[]) {
-    this.logger.log(`Send Data to Microservice: ${data.toString()}`);
+    // private client = ClientProxyFactory.create(this.microserviceOptions);
 
-    // return this.client.send<number, number[]>('add', [1, 2, 3]);
-    return await this.mathService.accumulate(data);
-  }
+    onModuleInit() {
+        this.grpcService =
+            this.client.getService<IGrpcService>('AppController');
+    }
+
+    // @Get()
+    // getHello(): string {
+    //   return this.appService.getHello();
+    // }
+
+    // // Map the POST /add routeto this method
+    // @Post('add')
+    // // define the logic to be executed
+    // async accumulate(@Body('data') data: number[]) {
+    //   this.logger.log(`Send Data to Microservice: ${data.toString()}`);
+
+    //   // return this.client.send<number, number[]>('add', [1, 2, 3]);
+    //   return this.mathService.accumulate(data);
+    // }
+
+    @Post('add')
+    async accumulate(@Body('data') data: number[]) {
+        this.logger.log(`Send Data to Microservice: ${data.toString()}`);
+
+        // return this.client.send<number, number[]>('add', [1, 2, 3]);
+        return this.grpcService.accumulate({ data });
+    }
 }
