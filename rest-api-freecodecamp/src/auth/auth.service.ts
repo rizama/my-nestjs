aa0dffe514d75a01a4a06bdfb5f1f25116a266ac
rizkysampratama <rizkysampratama@gmail.com>
2022-06-04 23:20:09 +0700

    add package generator hash

--- rest-api-freecodecamp/src/auth/auth.service.ts
+++ rest-api-freecodecamp/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -8,6 +9,12 @@ export class AuthService {
     constructor(private prisma: PrismaService){}
 
     signup(dto: AuthDto) {
+        // generate the password hash
+
+        // save the new user
+
+        // return the saved user
+
         return { msg: 'signup' };
     }
 
