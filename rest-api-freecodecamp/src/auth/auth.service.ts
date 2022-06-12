1b6df52c361ec67bb4b6eaec24cd403b745abdf8
rizkysampratama <rizkysampratama@gmail.com>
2022-06-12 21:24:16 +0700

    update signup logic

--- rest-api-freecodecamp/src/auth/auth.service.ts
+++ rest-api-freecodecamp/src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
@@ -9,24 +10,32 @@ export class AuthService {
     constructor(private prisma: PrismaService){}
 
     async signup(dto: AuthDto) {
-        // generate the password hash
-        const hash = await argon.hash(dto.password);
 
-        // save the new user
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash,
-            },
-            select: {
-                id: true,
-                email: true,
-                createdAt: true,
+        try {
+            // generate the password hash
+            const hash = await argon.hash(dto.password);
+    
+            // save the new user
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                }
+            })
+    
+            delete user.hash;
+    
+            // return the saved user
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials Taken');
+                }
             }
-        })
 
-        // return the saved user
-        return user;
+            throw error;
+        }
     }
 
     signin() {
