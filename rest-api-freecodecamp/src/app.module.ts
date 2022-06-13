4396e2e0b7d67487ca18517c25507c5062a21474
rizkysampratama <rizkysampratama@gmail.com>
2022-06-13 23:37:39 +0700

    add config module

--- rest-api-freecodecamp/src/app.module.ts
+++ rest-api-freecodecamp/src/app.module.ts
@@ -3,10 +3,19 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
-  controllers: [],
-  providers: [],
+    imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+        }),
+        AuthModule, 
+        UserModule, 
+        BookmarkModule, 
+        PrismaModule
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
--- rest-api-freecodecamp/src/prisma/prisma.service.ts
+++ rest-api-freecodecamp/src/prisma/prisma.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
+    constructor(
+        configService: ConfigService
+    ) {
         super({
             datasources: {
                 db: {
-                    url: 'postgresql://postgres:postgres@localhost:5433/nest?schema=public'
+                    url: configService.get('DATABASE_URL')
                 }
             }
         })
