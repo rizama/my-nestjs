90a5e77bc4ba13bf14f13597c440249367ac5509
rizkysampratama <rizkysampratama@gmail.com>
2022-08-09 22:50:38 +0700

    add signup api

--- basic-auth-local-session/src/users/users.controller.ts
+++ basic-auth-local-session/src/users/users.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { UsersService } from './users.service';
+import * as bcrypt from 'bcrypt';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+    //post / signup
+    @Post('/signup')
+    async addUser(
+        @Body('password') userPassword: string,
+        @Body('username') userName: string,
+    ) {
+        const saltOrRounds = 10;
+        const hashedPassword = await bcrypt.hash(userPassword, saltOrRounds);
+        const result = await this.usersService.insertUser(
+            userName,
+            hashedPassword,
+        );
+        return {
+            msg: 'User successfully registered',
+            userId: result.id,
+            userName: result.username,
+        };
+    }
+}
