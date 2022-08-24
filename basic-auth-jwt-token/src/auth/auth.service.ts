bead7169676442175a43adb919e2b02a483f319c
Sam <rizky.pratama@sicepat.com>
2022-08-24 09:16:23 +0700

    fix payload varified

--- basic-auth-jwt-token/src/auth/auth.service.ts
+++ basic-auth-jwt-token/src/auth/auth.service.ts
@@ -33,7 +33,7 @@ export class AuthService {
     }
 
     async login(user: any) {
-        const payload = { username: user.userName, sub: user.userId };
+        const payload = { username: user.username, sub: user.id };
 
         return {
             access_token: this.jwtService.sign(payload),
